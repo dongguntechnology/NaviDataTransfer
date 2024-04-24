9c1bad926dd643ca0dd949c18f90deb6d3d31d9a
dongguntechnology <shafeel2@naver.com>
2024-04-24 16:58:24 +0900

    Initial commit

---
+++ app/src/main/java/com/dongguninnovatiion/navidatatransfer/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.dongguninnovatiion.navidatatransfer
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+// https://developer.android.com/guide/navigation/use-graph/pass-data?hl=ko
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
